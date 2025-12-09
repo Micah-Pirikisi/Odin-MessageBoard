269cdf423c4548a5cb5259be9d97ae1c3e6d4076
Micah-Pirkisi <micahpirikisi@gmail.com>
2025-12-09 16:19:42 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// Index route
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home Page" });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
