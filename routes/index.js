b7fbbb42223c7a92c6cc18132f10f52001cc376b
Micah-Pirkisi <micahpirikisi@gmail.com>
2025-12-09 16:34:46 +0200

    Created sample messages in index.js with input field for new messages and added message loop in index.ejs

---
+++ routes/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+router.get("/", (req, res) => {
+  res.render("index", {
+    title: "Mini Messageboard",
+    messages: messages,
+  });
+});
+
+router.get("/new", (req, res) => {
+  res.render("form", { title: "New Message" });
+});
+
+router.post('/new', (req, res) => {
+  const { user, text } = req.body;
+  
+  messages.push({
+    text: text,
+    user: user,
+    added: new Date()
+  });
+
+  res.redirect('/');
+});
+
+
+module.exports = router;
